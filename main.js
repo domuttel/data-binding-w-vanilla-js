98a129cf64a51d4273d326c476c48d4ebd6c3061
Dominic Muttel <dominic.muttel@gmail.com>
2015-10-26 15:07:07 -0600

    added dom function

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 // ** globals ** //
 
 var scope = {};
-//Array of classes to add the data binding to
+// ** Array of classes to add the data binding to ** //
 var classNamesArray = ['name'];
 
 // ** functions ** //
+
+function getElements(){
+  var outputArray = [];
+  for (var i = 0; i < classNamesArray.length; i++) {
+    outputArray.push(document.getElementsByClassName(classNamesArray[i])[0]);
+  }
+  return outputArray;
+}
+
+// ** function calls ** //
+
+var domElement = getElements();
